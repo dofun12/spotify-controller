893469d5a876d771361932f88a1d5acd8469d49d
kvnsu <k.v.n.such@gmail.com>
2022-11-23 17:43:16 -0300

    Scheduler update

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from apscheduler.triggers.interval import IntervalTrigger
-from fastapi import FastAPI
-import asyncio
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from routes.api import router as api_router
-from fastapi.middleware.cors import CORSMiddleware
+from datetime import date
 
-app = FastAPI()
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi_amis_admin.admin.settings import Settings
+from fastapi_amis_admin.admin.site import AdminSite
+from fastapi_scheduler import SchedulerAdmin
+
+from routes.api import router as api_router
 
 
 def get_application() -> FastAPI:
@@ -16,30 +18,50 @@ def get_application() -> FastAPI:
     return application
 
 
+site = AdminSite(settings=Settings(database_url_async='sqlite+aiosqlite:///amisadmin.db'))
+
+# # Custom timed task scheduler
+# from apscheduler.schedulers.asyncio import AsyncIOScheduler
+# from apscheduler.jobstores.redis import RedisJobStore
+# # Use `RedisJobStore` to create a job store
+# scheduler = AsyncIOScheduler(jobstores={'default':RedisJobStore(db=2,host="127.0.0.1",port=6379,password="test")})
+# scheduler = SchedulerAdmin.bind(site,scheduler=scheduler)
+
+# Create an instance of the scheduled task scheduler `SchedulerAdmin`
+scheduler = SchedulerAdmin.bind(site)
 app = get_application()
+
+
+# Add scheduled tasks, refer to the official documentation: https://apscheduler.readthedocs.io/en/master/
+# use when you want to run the job at fixed intervals of time
+@scheduler.scheduled_job('interval', seconds=60)
+def interval_task_test():
+    print('interval task is run...')
+
+
+# use when you want to run the job periodically at certain time(s) of day
+@scheduler.scheduled_job('cron', hour=3, minute=30)
+def cron_task_test():
+    print('cron task is run...')
+
+
+# use when you want to run the job just once at a certain point of time
+@scheduler.scheduled_job('date', run_date=date(2022, 11, 11))
+def date_task_test():
+    print('date task is run...')
+
+
+@app.on_event("startup")
+async def startup():
+    # Mount the background management system
+    site.mount_app(app)
+    # Start the scheduled task scheduler
+    scheduler.start()
+
+
 origins = [
     '*'
 ]
-
-class SchedulerMiddleware:
-    def __init__(
-        self,
-        app: ASGIApp,
-        scheduler: AsyncScheduler,
-    ) -> None:
-        self.app = app
-        self.scheduler = scheduler
-
-    async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
-        if scope["type"] == "lifespan":
-            async with self.scheduler:
-                await self.scheduler.add_schedule(
-                    tick, IntervalTrigger(seconds=1), id="tick"
-                )
-                await self.scheduler.start_in_background()
-                await self.app(scope, receive, send)
-        else:
-            await self.app(scope, receive, send)
 
 
 app.add_middleware(
