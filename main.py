034566cc41c587040568508156d09288d5317b58
kevim <k.v.n.such@gmail.com>
2022-11-24 20:08:31 -0300

    Docker build

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from datetime import date
 
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi_amis_admin.admin.settings import Settings
@@ -18,22 +17,11 @@ def get_application() -> FastAPI:
     return application
 
 
-site = AdminSite(settings=Settings(database_url_async='sqlite+aiosqlite:///amisadmin.db'))
-
-# # Custom timed task scheduler
-# from apscheduler.schedulers.asyncio import AsyncIOScheduler
-# from apscheduler.jobstores.redis import RedisJobStore
-# # Use `RedisJobStore` to create a job store
-# scheduler = AsyncIOScheduler(jobstores={'default':RedisJobStore(db=2,host="127.0.0.1",port=6379,password="test")})
-# scheduler = SchedulerAdmin.bind(site,scheduler=scheduler)
-
-# Create an instance of the scheduled task scheduler `SchedulerAdmin`
-scheduler = SchedulerAdmin.bind(site)
 app = get_application()
+site = AdminSite(settings=Settings(database_url_async='sqlite+aiosqlite:///amisadmin.db'))
+scheduler = SchedulerAdmin.bind(site)
 
 
-# Add scheduled tasks, refer to the official documentation: https://apscheduler.readthedocs.io/en/master/
-# use when you want to run the job at fixed intervals of time
 @scheduler.scheduled_job('interval', seconds=60)
 def interval_task_test():
     print('interval task is run...')
@@ -63,7 +51,6 @@ origins = [
     '*'
 ]
 
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -71,3 +58,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+if __name__ == '__main__':
+    import uvicorn
+
+    uvicorn.run(app)
